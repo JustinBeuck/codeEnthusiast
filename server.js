26b08688a93954e03a9f2ff57428cdf6ea56055e
Justin Beuck <jbeuck@yahoo.com>
2016-05-13 16:02:39 -0500

    added server.js code to deploy on heroku

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ require('./app/routes')(app); // pass our application into our routes
 // start app ===============================================
 app.listen(port);	
 
-console.log('Magic happens on port ' + port); 			// shoutout to the user
+var port=Number(process.env.PORT || port);			// shoutout to the user
 exports = module.exports = app; 						// expose app
