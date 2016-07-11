150a74428f9cba6664306c6edf906aa847b97295
Justin Beuck <jbeuck@yahoo.com>
2016-07-10 22:01:03 -0500

    added register function to service

--- public/js/services.js
+++ public/js/services.js
@@ -12,13 +12,10 @@ angular.module('myApp').factory('AuthService',
 
 		function login(username, password) {
 
-		  // create a new instance of deferred
 		  var deferred = $q.defer();
 
-		  // send a post request to the server
 		  $http.post('/user/login',
 		    {username: username, password: password})
-		    // handle success
 		    .success(function (data, status) {
 		      if(status === 200 && data.status){
 		        user = true;
@@ -28,13 +25,50 @@ angular.module('myApp').factory('AuthService',
 		        deferred.reject();
 		      }
 		    })
-		    // handle error
 		    .error(function (data) {
 		      user = false;
 		      deferred.reject();
 		    });
 
-		  // return promise object
+		  return deferred.promise;
+
+		}
+
+		function logout() {
+
+		  var deferred = $q.defer();
+
+		  $http.get('/user/logout')
+		    .success(function (data) {
+		      user = false;
+		      deferred.resolve();
+		    })
+		    .error(function (data) {
+		      user = false;
+		      deferred.reject();
+		    });
+
+		  return deferred.promise;
+
+		}
+
+		function register(username, password) {
+
+		  var deferred = $q.defer();
+
+		  $http.post('/user/register',
+		    {username: username, password: password})
+		    .success(function (data, status) {
+		      if(status === 200 && data.status){
+		        deferred.resolve();
+		      } else {
+		        deferred.reject();
+		      }
+		    })
+		    .error(function (data) {
+		      deferred.reject();
+		    });
+
 		  return deferred.promise;
 
 		}
